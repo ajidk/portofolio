22d8bbd52ee0bfcbfe532e195441c4ef08350217
Surajidk <surajidk12@gmail.com>
2022-05-15 13:20:09 +0700

    update button skill

--- components/Resume/Skills.js
+++ components/Resume/Skills.js
@@ -9,15 +9,15 @@ export default function Skills() {
       </div>
       <Gap height={24} />
       <div className="flex flex-wrap justify-center items-center">
-        <button className="bg-gray-100 text-sm uppercase py-2 px-4 m-4">laravel</button>
-        <button className="bg-gray-100 text-sm uppercase py-2 px-4 m-4">react js</button>
-        <button className="bg-gray-100 text-sm uppercase py-2 px-4 m-4">wordpress</button>
-        <button className="bg-gray-100 text-sm uppercase py-2 px-4 m-4">git</button>
-        <button className="bg-gray-100 text-sm uppercase py-2 px-4 m-4">html</button>
-        <button className="bg-gray-100 text-sm uppercase py-2 px-4 m-4">css</button>
-        <button className="bg-gray-100 text-sm uppercase py-2 px-4 m-4">react native</button>
-        <button className="bg-gray-100 text-sm uppercase py-2 px-4 m-4">tailwind</button>
-        <button className="bg-gray-100 text-sm uppercase py-2 px-4 m-4">bootstrap</button>
+        <button className="bg-gray-100 text-sm uppercase py-2 px-4 xs:m-1 m-4">laravel</button>
+        <button className="bg-gray-100 text-sm uppercase py-2 px-4 xs:m-1 m-4">react js</button>
+        <button className="bg-gray-100 text-sm uppercase py-2 px-4 xs:m-1 m-4">wordpress</button>
+        <button className="bg-gray-100 text-sm uppercase py-2 px-4 xs:m-1 m-4">git</button>
+        <button className="bg-gray-100 text-sm uppercase py-2 px-4 xs:m-1 m-4">html</button>
+        <button className="bg-gray-100 text-sm uppercase py-2 px-4 xs:m-1 m-4">css</button>
+        <button className="bg-gray-100 text-sm uppercase py-2 px-4 xs:m-1 m-4">react native</button>
+        <button className="bg-gray-100 text-sm uppercase py-2 px-4 xs:m-1 m-4">tailwind</button>
+        <button className="bg-gray-100 text-sm uppercase py-2 px-4 xs:m-1 m-4">bootstrap</button>
       </div>
     </div>
   );
